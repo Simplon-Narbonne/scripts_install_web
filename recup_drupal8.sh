2b93ebb4febf6962f9f7ecb9f4a4be1736ec9b16
root <root@localhost.localdomain>
2016-09-05 18:51:14 +0200

    Detecte si l'archive a déjà été télécharger et désarchiver pour ne pas le refaire la deuxieme fois

--- recup_drupal8.sh
+++ recup_drupal8.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 nomsite=$1
 version=8.1.8
-wget https://www.drupal.org/files/projects/drupal-$version.tar.gz
-tar -zxvf drupal-$version.tar.gz
+if [ ! -f ./drupal-$version.tar.gz ]
+then 
+    wget https://www.drupal.org/files/projects/drupal-$version.tar.gz
+    tar -zxvf drupal-$version.tar.gz
+fi
 mkdir /var/www/html/$nomsite
-mv drupal-$version/* /var/www/html/$nomsite/
+cp -ra drupal-$version/* /var/www/html/$nomsite/
+cp -ra drupal-$version/.htaccess /var/www/html/$nomsite/
 
 cp /var/www/html/$nomsite/sites/default/default.settings.php /var/www/html/$nomsite/sites/default/settings.php
 chmod a+w /var/www/html/$nomsite/sites/default/settings.php
 mkdir -p /var/www/html/$nomsite/sites/default/files/translations
 chmod -R a+w /var/www/html/$nomsite/sites/default/files
 
-mysql -u 'root' -p create $nomsite
+mysqladmin -u 'root' -p create $nomsite
+
+echo "http://localhost/$nomsite"
