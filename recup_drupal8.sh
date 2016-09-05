4f425c8e34eee6f5a992aff5854b6ec21079416f
root <root@localhost.localdomain>
2016-09-05 18:02:08 +0200

    Mise en variable du numéro de version et changement de la commande mysql plus universel

--- recup_drupal8.sh
+++ recup_drupal8.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 nomsite=$1
- wget https://www.drupal.org/files/projects/drupal-8.1.0.tar.gz
+version=8.1.8
+wget https://www.drupal.org/files/projects/drupal-$version.tar.gz
+tar -zxvf drupal-$version.tar.gz
+mkdir /var/www/html/$nomsite
+mv drupal-$version/* /var/www/html/$nomsite/
 
-  tar -zxvf drupal-8.1.0.tar.gz
+cp /var/www/html/$nomsite/sites/default/default.settings.php /var/www/html/$nomsite/sites/default/settings.php
+chmod a+w /var/www/html/$nomsite/sites/default/settings.php
+mkdir -p /var/www/html/$nomsite/sites/default/files/translations
+chmod -R a+w /var/www/html/$nomsite/sites/default/files
 
- mv drupal-8.1.0 /var/www/html/$nomsite
-
- cp /var/www/html/$nomsite/sites/default/default.settings.php /var/www/html/$nomsite/sites/default/settings.php
- chmod a+w /var/www/html/$nomsite/sites/default/settings.php
- mkdir -p /var/www/html/$nomsite/sites/default/files/translations
- chmod -R a+w /var/www/html/$nomsite/sites/default/files
-
- mysqladmin -u 'root' -p create $nomsite
+mysql -u 'root' -p create $nomsite
